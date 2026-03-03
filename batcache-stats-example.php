9063fc413f36923f30c0a57075da505c76465f91
Amaury Balmer <amaury@beapi.fr>
2026-03-03 09:45:42 +0100

    Refactor Batcache code for improved readability and PHP compatibility
    - Updated function definitions and conditionals for better clarity and consistency.
    - Enhanced comments for better understanding of functionality.
    - Adjusted formatting and spacing for improved code style.
    - Ensured compatibility with PHP 8.2 by declaring used properties and using modern syntax.

--- batcache-stats-example.php
+++ batcache-stats-example.php
@@ -1,32 +1,55 @@
 <?php
-
 /**
+ * Example Batcache stats implementation (copy and customize as batcache-stats.php).
+ *
  * This is a copy of our Batcache-stats.php file but it is suffixed so that you don't use it by default.
- * You probably don't want huge log files filling up your server by default, but this gives you and idea
+ * You probably don't want huge log files filling up your server by default, but this gives you an idea
  * of how we use this function. We have a separate parsing script that comes along and reads these files
  * and enters them into our internal stats.
+ *
+ * @package Batcache
  */
 
-if ( !function_exists( 'batcache_stats' ) ) {
-	function batcache_stats( $name, $value, $num = 1, $today = FALSE, $hour = FALSE ) {
-		if ( !$today )
+if ( ! function_exists( 'batcache_stats' ) ) {
+	/**
+	 * Logs a stat value to a local file (example implementation).
+	 *
+	 * Batcache never loads wpdb, so stats are written asynchronously to a file.
+	 *
+	 * @param string       $name  Stat name.
+	 * @param string       $value Stat value.
+	 * @param int          $num   Optional. Count. Default 1.
+	 * @param string|false $today Optional. Date for filename. Default false (today).
+	 * @param string|false $hour  Optional. Hour for log line. Default false (current hour).
+	 */
+	function batcache_stats( $name, $value, $num = 1, $today = false, $hour = false ) {
+		if ( ! $today ) {
 			$today = gmdate( 'Y-m-d' );
-		if ( !$hour )
+		}
+		if ( ! $hour ) {
 			$hour = gmdate( 'Y-m-d H:00:00' );
+		}
 
-		// batcache never loads wpdb so always do this async
-		if ( !file_exists( '/var/spool/wpcom/extra' ) )
+		// Batcache never loads wpdb so always do this async.
+		if ( ! file_exists( '/var/spool/wpcom/extra' ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_mkdir -- Example: custom stats path outside WP.
 			mkdir( '/var/spool/wpcom/extra', 0777 );
+		}
 
-		$value = rawurlencode( $value );
-		$stats_filename = "/var/spool/wpcom/extra/{$today}_" . gmdate( 'H' . '-' . 'i' );
+		$value          = rawurlencode( $value );
+		$stats_filename = "/var/spool/wpcom/extra/{$today}_" . gmdate( 'H-i' );
 		if ( ! file_exists( $stats_filename ) ) {
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_touch -- Example: custom stats file.
 			touch( $stats_filename );
+			// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_chmod -- Example: custom stats file.
 			chmod( $stats_filename, 0777 );
 		}
 
-		$fp = fopen( $stats_filename,  'a' );
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_fopen -- Example: append-only stats log.
+		$fp = fopen( $stats_filename, 'a' );
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_fwrite -- Example: append-only stats log.
 		fwrite( $fp, "{$hour}\t{$name}\t{$value}\t{$num}" . chr( 10 ) );
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_fclose -- Example: append-only stats log.
 		fclose( $fp );
 	}
 }
--- batcache.php
+++ batcache.php
@@ -1,60 +1,76 @@
 <?php
-/*
-Plugin name: Batcache Manager
-Plugin URI: http://wordpress.org/extend/plugins/batcache/
-Description: This optional plugin improves Batcache.
-Author: Andy Skelton
-Author URI: http://andyskelton.com/
-Version: 1.5
-*/
+/**
+ * Plugin name: Batcache Manager
+ * Plugin URI: http://wordpress.org/extend/plugins/batcache/
+ * Description: This optional plugin improves Batcache.
+ * Author: Andy Skelton
+ * Author URI: http://andyskelton.com/
+ * Version: 1.5
+ *
+ * @package Batcache
+ */
 
-// Do not load if our advanced-cache.php isn't loaded
-if ( ! isset( $batcache ) || ! is_object($batcache) || ! method_exists( $wp_object_cache, 'incr' ) )
+// Do not load if our advanced-cache.php isn't loaded.
+if ( ! isset( $batcache ) || ! is_object( $batcache ) || ! method_exists( $wp_object_cache, 'incr' ) ) {
 	return;
+}
 
 $batcache->configure_groups();
 
-// Regen home and permalink on posts and pages
-add_action('clean_post_cache', 'batcache_post', 10, 2);
+// Regen home and permalink on posts and pages.
+add_action( 'clean_post_cache', 'batcache_post', 10, 2 );
 
-// Regen permalink on comments (TODO)
-//add_action('comment_post',          'batcache_comment');
-//add_action('wp_set_comment_status', 'batcache_comment');
-//add_action('edit_comment',          'batcache_comment');
+// Optional: regen permalink on comment post, status change, or edit (uncomment add_action calls as needed).
 
-function batcache_post($post_id, $post = null) {
+/**
+ * Clears batcache for the given URLs when a post is updated.
+ *
+ * @param int          $post_id Post ID.
+ * @param WP_Post|null $post    Post object. Optional, for backwards compatibility.
+ */
+function batcache_post( $post_id, $post = null ) {
 	global $batcache;
 
-	// Get the post for backwards compatibility with earlier versions of WordPress
+	// Get the post for backwards compatibility with earlier versions of WordPress.
 	if ( ! $post ) {
-		$post = get_post( $post_id );	
+		$post = get_post( $post_id );
 	}
-	
-	if ( ! $post || $post->post_type == 'revision' || ! in_array( get_post_status($post_id), array( 'publish', 'trash' ) ) )
-		return;
 
-	$home = trailingslashit( get_option('home') );
+	if ( ! $post || 'revision' === $post->post_type || ! in_array( get_post_status( $post_id ), array( 'publish', 'trash' ), true ) ) {
+		return;
+	}
+
+	$home = trailingslashit( get_option( 'home' ) );
 	batcache_clear_url( $home );
 	batcache_clear_url( $home . 'feed/' );
-	batcache_clear_url( get_permalink($post_id) );
+	batcache_clear_url( get_permalink( $post_id ) );
 }
 
-function batcache_clear_url($url) {
+/**
+ * Clears batcache for a given URL by incrementing its version key.
+ *
+ * @param string $url URL to clear from cache.
+ * @return int|false Cache version after increment on success, false on failure.
+ */
+function batcache_clear_url( $url ) {
 	global $batcache, $wp_object_cache;
 
-	if ( empty($url) )
+	if ( empty( $url ) ) {
 		return false;
+	}
 
-	if ( 0 === strpos( $url, 'https://' ) )
+	if ( 0 === strpos( $url, 'https://' ) ) {
 		$url = str_replace( 'https://', 'http://', $url );
-	if ( 0 !== strpos( $url, 'http://' ) )
+	}
+	if ( 0 !== strpos( $url, 'http://' ) ) {
 		$url = 'http://' . $url;
+	}
 
 	$url_key = md5( $url );
-	wp_cache_add("{$url_key}_version", 0, $batcache->group);
-	$retval = wp_cache_incr("{$url_key}_version", 1, $batcache->group);
+	wp_cache_add( "{$url_key}_version", 0, $batcache->group );
+	$retval = wp_cache_incr( "{$url_key}_version", 1, $batcache->group );
 
-	$batcache_no_remote_group_key = array_search( $batcache->group, (array) $wp_object_cache->no_remote_groups );
+	$batcache_no_remote_group_key = array_search( $batcache->group, (array) $wp_object_cache->no_remote_groups, true );
 	if ( false !== $batcache_no_remote_group_key ) {
 		// The *_version key needs to be replicated remotely, otherwise invalidation won't work.
 		// The race condition here should be acceptable.
